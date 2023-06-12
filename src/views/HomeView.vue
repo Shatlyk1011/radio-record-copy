5dd2a1b497909a5bc56c03d1014ebdc15618c087
Shatlyk Abdullayev <shatlykabdullayev@main.ru>
2023-06-12 18:57:55 +0300

    add menu of three items

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,19 @@
 <template>
   <main class="homeView">
     <SliderComponent />
+    <div class="wrap padding-10-x">
+      <MenuThree />
+    </div>
   </main>
 </template>
 
 <script setup>
 import SliderComponent from '@/components/SliderComponent.vue'
+import MenuThree from '@/components/shared/MenuThree.vue'
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.wrap {
+  margin: 4.8rem 0 3.2rem;
+}
+</style>
