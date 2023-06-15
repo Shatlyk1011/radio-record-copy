f908d56e3751ea6ef19058c5500e6042355ef984
Shatlyk Abdullayev <shatlykabdullayev@main.ru>
2023-06-15 12:26:33 +0300

    add useStation composable

--- src/composables/useStation.ts
+++ src/composables/useStation.ts
@@ -1,16 +1,32 @@
-import { ref, type Ref } from 'vue'
-import type { IStation } from '@/assets/types'
+import { ref } from 'vue'
+import type { IStation, IPlaylist } from '@/assets/types'
+
+import { stations } from '@/db'
 
 const channel = ref<IStation>()
+const playlist = ref<IPlaylist>()
 
-const isPending = ref(false)
-const handleStation = (station: IStation) => {
-  channel.value = station
-  isPending.value = true
+const getPlaylist = async (id: number) => {
+  const response = await fetch('https://www.radiorecord.ru/api/stations/now/')
+
+  const data = await response.json()
+  const { result } = data
+  result.filter((res: IPlaylist) => {
+    if (res.id == id) playlist.value = res
+  })
 }
 
+const handleStation = async (station?: IStation) => {
+  if (station) {
+    channel.value = station
+    await getPlaylist(channel.value.id)
+  } else {
+    channel.value = stations[0]
+    await getPlaylist(stations[0].id)
+  }
+}
 const useStation = () => {
-  return { channel, handleStation, isPending }
+  return { channel, playlist, handleStation, getPlaylist }
 }
 
 export default useStation
